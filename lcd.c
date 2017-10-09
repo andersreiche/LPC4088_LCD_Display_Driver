ac5e6e570659b4765d1c8faef4cde283b9f3cd55
Anders Reiche <andersreiche@gmail.com>
2017-10-09 14:20:43 +0200

    all modules in place, need to write test and define pins / ports

--- lcd.c
+++ lcd.c
@@ -1,41 +1,49 @@
 #include "board.h"
 #include "lcd.h"
 
-
 /*LCD data pins and ports*/
-#define DATA_PORT0
-#define DATA_PORT2
-#define DATA_PORT3
-#define DATA_PORT4
-#define DATA_PORT5
-#define DATA_PORT6
-#define DATA_PORT7
-#define DATA_PORT8
-#define DATA_PIN1
-#define DATA_PIN2
-#define DATA_PIN3
-#define DATA_PIN4
-#define DATA_PIN5
-#define DATA_PIN6
-#define DATA_PIN7
-#define DATA_PIN0
+#define DATA_PORT1 0
+#define DATA_PORT2 0
+#define DATA_PORT3 0
+#define DATA_PORT4 0
+#define DATA_PORT5 0
+#define DATA_PORT6 0
+#define DATA_PORT7 0
+#define DATA_PORT0 0
+#define DATA_PIN1 0
+#define DATA_PIN2 0
+#define DATA_PIN3 0
+#define DATA_PIN4 0
+#define DATA_PIN5 0
+#define DATA_PIN6 0
+#define DATA_PIN7 0
+#define DATA_PIN0 0
 
 /*LCD command pins and ports*/
-#define ENABLE_PORT
-#define ENABLE_PIN
-#define RS_PORT
-#define RS_PIN
-#define ENABLE_HIGH() Chip_GPIO_SetPinState(LPC_GPIO, ENABLE_PORT, ENABLE_PIN, 1);
-#define ENABLE_LOW() Chip_GPIO_SetPinState(LPC_GPIO, ENABLE_PORT, ENABLE_PIN, 0);
+#define ENABLE_PORT 0
+#define ENABLE_PIN 0
+#define RS_PORT 0
+#define RS_PIN 0
+#define RW_PORT 0
+#define RW_PIN 0
+#define ENABLE_READY() Chip_GPIO_SetPinState(LPC_GPIO, ENABLE_PORT, ENABLE_PIN, 1);
+#define ENABLE_SEND() Chip_GPIO_SetPinState(LPC_GPIO, ENABLE_PORT, ENABLE_PIN, 0);
 #define RS_HIGH() Chip_GPIO_SetPinState(LPC_GPIO, RS_PORT, RS_PIN, 1);
 #define RS_LOW() Chip_GPIO_SetPinState(LPC_GPIO, RS_PORT, RS_PIN, 0);
+#define RW_W() Chip_GPIO_SetPinState(LPC_GPIO, RW_PORT, RW_PIN, 0);
+#define RW_R() Chip_GPIO_SetPinState(LPC_GPIO, RW_PORT, RW_PIN, 1);
 
-#define CMD_PORT3
-#define CMD_PIN1
-#define CMD_PIN2
-#define CMD_PIN3
-
- // Chip_GPIO_SetPinState(LPC_GPIO, PORT, PIN, STATE(1,0));
+void setbus(char c) {
+	/* set bits 0-7 according to char*/
+	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT0, DATA_PIN0, c & 0b00000001);
+	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT1, DATA_PIN1, c & 0b00000010);
+	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT2, DATA_PIN2, c & 0b00000100);
+	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT3, DATA_PIN3, c & 0b00001000);
+	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT4, DATA_PIN4, c & 0b00010000);
+	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT5, DATA_PIN5, c & 0b00100000);
+	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT6, DATA_PIN6, c & 0b01000000);
+	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT7, DATA_PIN7, c & 0b10000000);
+}
 
 void commandmode(void) {
 	RS_LOW()
@@ -46,54 +54,46 @@ void datamode(void) {
 }
 
 void lcd_init(void) {
-	// Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT0, DATA_PIN0, 1);
-	// set R/W = W
-}
-
-void lcd_write() { // 8 bit databus
-
+	RW_W() // sets the display to write mode, this will never change
+	lcd_write_cmd(LCD_FORMAT);
+	lcd_write_cmd(LCD_CLEAR);
 
 }
 
-
-void lcd_set_cursor (int column, int row) {
-
+void lcd_write_data(char c) {
+	datamode();
+	ENABLE_READY()
+	setbus(c);
+	delay_us(1);
+	ENABLE_SEND()
 }
 
-void lcd_clear (void) {
-	// clear = 00000001
-	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT0, DATA_PIN0, 1);
-	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT1, DATA_PIN1, 0);
-	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT2, DATA_PIN2, 0);
-	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT3, DATA_PIN3, 0);
-	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT4, DATA_PIN4, 0);
-	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT5, DATA_PIN5, 0);
-	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT6, DATA_PIN6, 0);
-	Chip_GPIO_SetPinState(LPC_GPIO, DATA_PORT7, DATA_PIN7, 0);
+void lcd_write_cmd(char c) {
+	commandmode();
+	ENABLE_READY()
+	setbus(c);
+	delay_us(1);
+	ENABLE_SEND()
 }
+
 
 void lcd_put_char(char c) {
 	lcd_write_data(c);
 }
 
 void lcd_print(char *string) {
-	while(*string) {
+	while (*string) {
 		lcd_put_char(*string++);
 	}
 }
 
-void lcd_set_cursor_visibile(int visible) {
-	lcd_write_cmd(0x0C | ((!!visible) << 1));
-}
-
 /* Very simple (inaccurate) delay function */
-static void delay(uint32_t i)
-{
-	while (i--) {}
+static void delay(uint32_t i) {
+	while (i--) {
+	}
 }
 
-/* Delay in miliseconds  (cclk = 120MHz) */
-static void delay_us(uint32_t ms)
-{
+/* Delay in mucroseconds  (cclk = 120MHz) */
+static void delay_us(uint32_t ms) {
 	delay(ms * 40);
 }
--- lcd.h
+++ lcd.h
@@ -6,19 +6,12 @@
 #ifndef LCD_H
 #define LCD_H
 
+#define LCD_CLEAR 0b00000001
+#define LCD_FORMAT 0b00111100
 /*! \brief Initialises the LCD module.
  */
 void lcd_init(void);
 
-/*! \brief Moves the cursor position to the specified location.
- *  \param column  Destination column for the cursor.
- *  \param row     Destination row for the cursor.
- */
-void lcd_set_cursor(int column, int row);
-
-/*! \brief Clears the LCD and relocates the cursor to {0,0}.
- */
-void lcd_clear(void);
 
 /*! \brief Prints the null terminated string to the LCD and
  *         increments the cursor.
@@ -35,7 +28,12 @@ void lcd_put_char(char c);
 /*! \brief Enables or disables visibility of the cursor.
  *  \param visible New visibility of the cursor.
  */
-void lcd_set_cursor_visibile(int visible);
+
+void lcd_write_data(char c);
+void lcd_write_cmd(char c);
+void displaymode8bit2lines();
+void commandmode(void);
+void datamode(void);
 
 static void delay(uint32_t i);
 static void delay_us(uint32_t ms);
